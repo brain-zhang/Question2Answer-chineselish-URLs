5d28784ee417f24b2c5acf950389e97c4e05969f
memoryboxes <memoryboxes@gmail.com>
2016-02-02 09:43:22 +0800

    add desc

--- qa-plugin.php
+++ qa-plugin.php
@@ -1,15 +1,15 @@
 <?php
 /*
-	Plugin Name: Greek-to-Greeklish URI conversion
-	Plugin URI: 
-	Plugin Description: Converts all greek locale chars to the corrensponding latin ones, thus avoiding the conversion to encoded URLs.
+	Plugin Name: Question2Answer chinese URI conversion
+	Plugin URI: https://github.com/memoryboxes/Question2Answer-chineselish-URLs
+	Plugin Description: Converts all chinese locale chars to the corrensponding latin ones, thus avoiding the conversion to encoded URLs. fork from https://github.com/stefkiourk/Question2Answer-greeklish-URLs
 	Plugin Version: 1.0
-	Plugin Date: 2013-03-03
-	Plugin Author: Kiourkoulis Stefanos
-	Plugin Author URI: http://stefki.com
-	Plugin License: GPLv2
+	Plugin Date: 2015-02-02
+	Plugin Author: memoryboxes
+	Plugin Author URI: http://memoryboxes.github.com
+	Plugin License: BSD2
 	Plugin Minimum Question2Answer Version: 1.5
-	Plugin Update Check URI: 
+	Plugin Update Check URI:
 */
 
 if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
